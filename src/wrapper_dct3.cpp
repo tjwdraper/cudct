74f7bd04e86844454fb047b690a5e61eb90cd043
Tom Draper <tjwdraper@gmail.com>
2026-09-23 15:35:56 +0200

    Implemented a 3d dct check between mirt and fftw.

---
+++ src/wrapper_dct3.cpp
@@ -0,0 +1,57 @@
+#include <mex.h>
+#include <matrix.h>
+#include <string>
+#include <fftw3.h>
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+    // Check number of input and output variables
+    if (nlhs != 1 || nrhs != 2)
+        mexErrMsgTxt("Invalud number of input and output variables given");
+
+    // Check data variable format
+    if (!mxIsDouble(prhs[0]) || mxIsComplex(prhs[0]))
+        mexErrMsgTxt("First input must be a real double matrix.");
+
+    // Load dimensions
+    const mxArray* data = prhs[0];
+    mwSize ndim = mxGetNumberOfDimensions(data);
+    if (ndim != 3) 
+        mexErrMsgTxt("First argument must be a 3d-matrix");
+    const mwSize* dims = mxGetDimensions(data);
+
+    // Get the input
+    double* input = mxGetPr(data);
+
+    // Allocate memory for the output-II and execute plan
+    plhs[0] = mxCreateNumericArray(3, dims, mxDOUBLE_CLASS, mxREAL);
+    double* output = mxGetPr(plhs[0]);
+
+    // Parse direction
+    if (!mxIsChar(prhs[1]))
+        mexErrMsgTxt("Second argument must be a character array.");
+    
+    char* operation = mxArrayToString(prhs[1]);
+    if (operation == nullptr)
+        mexErrMsgTxt("Could not convert to string.");
+
+    // Create fftw plan:
+    fftw_plan plan;
+    if (strcmp(operation, "forward") == 0)
+        plan = fftw_plan_r2r_3d(dims[2], dims[1], dims[0], input, output, FFTW_REDFT10, FFTW_REDFT10, FFTW_REDFT10, FFTW_ESTIMATE);
+    else if (strcmp(operation, "inverse") == 0)
+        plan = fftw_plan_r2r_3d(dims[2], dims[1], dims[0], input, output, FFTW_REDFT01, FFTW_REDFT01, FFTW_REDFT01, FFTW_ESTIMATE);
+    else {
+        mxFree(operation);
+        mexErrMsgTxt("Operation must be 'forward' or 'inverse'.");
+    }
+
+    if (plan == nullptr)
+        mexErrMsgTxt("Could not create FFTW plan.");
+
+    // (I)output calculation
+    fftw_execute(plan);
+
+    // Free memory
+    fftw_destroy_plan(plan);
+    mxFree(operation);
+}
--- tests/matlab_consistency_check_3d.m
+++ tests/matlab_consistency_check_3d.m
@@ -9,20 +9,31 @@ load mri;
 img = double(squeeze(D));
 clear D map siz;
 
-%% Calculate the forward DCT
-dct_img_matlab_standard = dct(img); 
+%% Calculate the forward DCT 
 dct_img_matlab_mirt = mirt_dctn(img);
 
+dct_img_fftw = matlab_dct3(img, 'forward');
+
+[M,N,K] = size(img);
+scale = ones(M,N,K) / (8 * sqrt(M*N*K));
+
+scale(2:end,:,:) = scale(2:end,:,:) * sqrt(2);
+scale(:,2:end,:) = scale(:,2:end,:) * sqrt(2);
+scale(:,:,2:end) = scale(:,:,2:end) * sqrt(2);
+
+dct_img_fftw = dct_img_fftw .* scale;
+
 %% Check differences between DCT transforms
-diff_dct_standard_mirt = sum((dct_img_matlab_standard(:) - dct_img_matlab_mirt(:)).^2);
-fprintf("Difference DCT standard - mirt:\t%.3f\n", diff_dct_standard_mirt);
+diff_dct_standard_fftw = sum((dct_img_fftw(:) - dct_img_matlab_mirt(:)).^2);
+fprintf("Difference DCT fftw - mirt:\t%.10f\n", diff_dct_standard_fftw);
 
 %% Calculate the inverse DCT
-recon_img_matlab_standard = idct(dct_img_matlab_standard);
 recon_img_matlab_mirt = mirt_idctn(dct_img_matlab_mirt);
+recon_img_fftw = matlab_dct3(matlab_dct3(img, 'forward'), 'inverse') / (8 * numel(img)); % Normalization factor
 
 %% Calculate the residual error
-err_matlab_standard = sum((recon_img_matlab_standard(:) - img(:)).^2);
 err_matlab_mirt = sum((recon_img_matlab_mirt(:) - img(:)).^2);
-fprintf("Error (standard):\t%.10f\n", err_matlab_standard);
-fprintf("Error (mirt):\t\t%.10f\n", err_matlab_mirt);
+err_fftw = sum((recon_img_fftw(:) - img(:)).^2);
+
+fprintf("Error (mirt):\t%.10f\n", err_matlab_mirt);
+fprintf("Error (fttw):\t%.10f\n", err_fftw);
